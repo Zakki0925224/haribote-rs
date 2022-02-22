92609f9d95eef9970faec3456022e3a4e8d7eea3
Zakki <zakki0925224@gmail.com>
2022-02-22 14:38:50 +0900

    [Day4][harib01h][Update]: Updated

--- src/lib.rs
+++ src/lib.rs
@@ -16,10 +16,30 @@ pub extern "C" fn haribote_os() -> !
 {
     set_palette(0, 15);
     let vram = unsafe { &mut *(0xa0000 as *mut u8) };
+    let xsize = 320;
+    let ysize = 200;
 
-    boxfill8(vram, 320, Color::LightRed, 20, 20, 120, 120);
-    boxfill8(vram, 320, Color::LightGreen, 70, 50, 170, 150);
-    boxfill8(vram, 320, Color::LightBlue, 120, 80, 220, 180);
+    // 背景
+    boxfill8(vram, xsize, Color::DarkCyan, 0, 0, xsize - 1, ysize - 29);
+
+    // タスクバー
+    boxfill8(vram, xsize, Color::LightGray, 0, ysize - 28, xsize - 1, ysize - 28);
+    boxfill8(vram, xsize, Color::White,     0, ysize - 27, xsize - 1, ysize - 27);
+    boxfill8(vram, xsize, Color::LightGray, 0, ysize - 26, xsize - 1, ysize - 1);
+
+    // スタートボタン？
+    boxfill8(vram, xsize, Color::White,    3, ysize - 24, 59, ysize - 24);
+    boxfill8(vram, xsize, Color::White,    2, ysize - 24, 2, ysize - 4);
+    boxfill8(vram, xsize, Color::DarkGray, 3, ysize - 4, 59, ysize - 4);
+    boxfill8(vram, xsize, Color::DarkGray, 59, ysize - 23, 59, ysize - 5);
+    boxfill8(vram, xsize, Color::Black,    2, ysize - 3, 59, ysize - 3);
+    boxfill8(vram, xsize, Color::Black,    60, ysize - 24, 60, ysize - 3);
+
+    // 通知
+    boxfill8(vram, xsize, Color::DarkGray, xsize - 47, ysize - 24, xsize - 4, ysize - 24);
+    boxfill8(vram, xsize, Color::DarkGray, xsize - 47, ysize - 23, xsize - 47, ysize - 4);
+    boxfill8(vram, xsize, Color::White,    xsize - 47, ysize - 3, xsize - 4, ysize - 3);
+    boxfill8(vram, xsize, Color::White,    xsize - 3, ysize - 24, xsize - 3, ysize - 3);
 
     loop { hlt(); }
 }
