d6ce526a82dc10175941f8b2de31eb16631adaac
Zakki0925224 <zakki0925224@gmail.com>
2022-02-23 23:21:45 +0900

    [Day5][harib02e][Todo]: The top few pixels of the font aren't displayed.

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,8 @@
 use core::{panic::PanicInfo};
 
 use asm::{hlt};
-use font::FONT_A;
-use vga::{Screen};
+use font::FONTPACK;
+use vga::{Screen, Color};
 
 mod asm;
 mod font;
@@ -18,7 +18,11 @@ pub extern "C" fn haribote_os() -> !
 {
     let mut screen = Screen::new();
     screen.init();
-    screen.pubfont8(10, 10, vga::Color::White, &FONT_A);
+    screen.pubfont8(10, 10, Color::White, &FONTPACK['@' as usize]);
+    screen.pubfont8(20, 10, Color::White, &FONTPACK['%' as usize]);
+    screen.pubfont8(30, 10, Color::White, &FONTPACK['$' as usize]);
+    screen.pubfont8(40, 10, Color::White, &FONTPACK['&' as usize]);
+    screen.pubfont8(50, 10, Color::White, &FONTPACK['Z' as usize]);
 
     loop { hlt(); }
 }
